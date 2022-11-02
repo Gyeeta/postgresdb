06e7def13bb52e9521f6da4368ef4829fa9fb888
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-02 13:13:58 +0530

    Force no cache
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -2,5 +2,5 @@
 
 POSTGRES_VERSION=`./rundb.sh --version | awk '{print $NF}'`
 
-DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/postgresdb:latest -t ghcr.io/gyeeta/postgresdb:"$POSTGRES_VERSION" -f ./Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/postgresdb:latest -t ghcr.io/gyeeta/postgresdb:"$POSTGRES_VERSION" --no-cache -f ./Dockerfile .
 
